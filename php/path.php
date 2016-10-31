8bc45b23692c5e5d2094a3f2e47da587fb9ae76f
Shoormax <valentin.derudet2@gmail.com>
2016-10-31 18:00:09 +0100

    Shadow sur la box des ML + set du local_path pour la prod

--- php/path.php
+++ php/path.php
@@ -5,11 +5,14 @@
  * Date: 21/09/2016
  * Time: 09:16
  */
+define("__LOCAL_PATH__", "http://monquartierconfluence.labo-g4.fr/MonQuartierConfluence");
 if(file_exists("C:/Users/Valentin")){
-    define("__LOCAL_PATH__", "http://localhost/ProjectOne");
-} else if(file_exists("C:/Users/quentin")){
+    define("__LOCAL_PATH__", "http://localhost/MQCG4");
+}
+else if(file_exists("C:/Users/quentin")){
     define("__LOCAL_PATH__", "http://localhost:8001/ProjectOne");
-} elseif( file_exists("C:/Users/AifeDesPaix") ) {
+}
+elseif( file_exists("C:/Users/AifeDesPaix") ) {
     define("__LOCAL_PATH__", "http://localhost/ProjectOne");
 }
 
