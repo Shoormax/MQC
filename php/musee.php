dc13658c97c50d3a5daf49a3d64020cc359cb15b
Quentin Benyahia <quentinbenyahia@gmail.com>
2016-11-02 16:33:46 +0100

    Debut Intégration

--- php/musee.php
+++ php/musee.php
@@ -13,6 +13,16 @@
         </div>
         <img class="imgArticle" id="articleArchiImg" src="img/min/musee.jpg">
     </div>
+    <div class="wrap-textuel">
+        <div class="sous-wrap-text">
+            <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis orci eget semper ornare. Etiam sed porttitor nisl. Nunc pretium semper dolor, vitae dapibus diam condimentum sed. Suspendisse tincidunt aliquet mauris, at auctor ante sagittis vel. Pellentesque vitae nisl nulla. Donec vitae accumsan orci. Duis quis enim sit amet eros consequat posuere. Quisque aliquet tincidunt sem, eu laoreet metus tristique tristique. Nunc enim leo, rhoncus vel vulputate sed, pellentesque at mi. Aliquam eget urna pulvinar, mollis ex ac, fermentum orci.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis orci eget semper ornare. Etiam sed porttitor nisl. Nunc pretium semper dolor, vitae dapibus diam condimentum sed. Suspendisse tincidunt aliquet mauris, at auctor ante sagittis vel. Pellentesque vitae nisl nulla. Donec vitae accumsan orci. Duis quis enim sit amet eros consequat posuere. Quisque aliquet tincidunt sem, eu laoreet metus tristique tristique. Nunc enim leo, rhoncus vel vulputate sed, pellentesque at mi. Aliquam eget urna pulvinar, mollis ex ac, fermentum orci
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis orci eget semper ornare. Etiam sed porttitor nisl. Nunc pretium semper dolor, vitae dapibus diam condimentum sed. Suspendisse tincidunt aliquet mauris, at auctor ante sagittis vel. Pellentesque vitae nisl nulla. Donec vitae accumsan orci. Duis quis enim sit amet eros consequat posuere. Quisque aliquet tincidunt sem, eu laoreet metus tristique tristique. Nunc enim leo, rhoncus vel vulputate sed, pellentesque at mi. Aliquam eget urna pulvinar, mollis ex ac, fermentum orci.
+            </span>
+        </div>
+    </div>
+
 
     <div class="clearfix"></div>
 
