ebc0c35a4906455cc4dcb243ddcbf00845423255
clapette <clapette01@gmail.com>
2016-11-03 12:05:24 +0100

    NECULEK DE TA E%R

--- php/contact.php
+++ php/contact.php
@@ -8,14 +8,16 @@
 ?>
 <div class="article" id="articleContact">
 	<h3>Contact</h3>
-	<div class="moitie fleft" id="contactInfos">
-		<input type="text" name="" placeholder="Nom">
-		<input type="text" name="" placeholder="Prénom">
-		<input id="mail_contact" type="mail" name="email" placeholder="E-m@il">
+	<div class="flexbox">
+		<div class="moitie" id="contactInfos">
+			<input type="text" name="" placeholder="Nom">
+			<input type="text" name="" placeholder="Prénom">
+			<input id="mail_contact" type="mail" name="email" placeholder="Email">
+		</div>
+		<div class="moitie">
+			<textarea id="messageContact" placeholder="Entrez votre message ici." rows="10"></textarea>
+			<input type="button" onclick="emailvalidation()" value="Envoyer">
+		</div>
 	</div>
-	<div class="moitie fleft">
-		<textarea id="messageContact" placeholder="Entrez votre message ici."></textarea>
-	</div>
-	<input type="button" onclick="emailvalidation()" value="Envoyer">
 	<div id="errorMail" class="hide"></div>
 </div>
--- php/mentions_legales.php
+++ php/mentions_legales.php
@@ -12,13 +12,13 @@ require_once (__INCLUDE_PATH__.'header.php');
 <div id="contenu_mentions_legales">
     <h1>Mentions légales</h1>
     <p>SITE ÉDITÉ PAR : Les Rescapés<br>
-        E-m@il : valentin.derudet2@gmail.com <br>
+        E-mail : valentin.derudet2@gmail.com <br>
         Téléphone : 06 18 14 19 84</p>
 
     Chef de projet : Valentin DERUDET<br>
     Développeurs :
     <br>- Quentin BENYAHIA
-    <br>- Joan TASSEL
+    <br>
     <br>- Thibault MOREY
     <br>- Boris SAINT-BONNET
     </p>
