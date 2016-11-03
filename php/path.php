bcb81bf50d78a7074e2569fc8a0d45b6acd68b51
clapette <clapette01@gmail.com>
2016-11-03 09:56:37 +0100

    ya

--- php/path.php
+++ php/path.php
@@ -8,19 +8,11 @@
 
 if(file_exists("C:/Users/Valentin")){
     define("__LOCAL_PATH__", "http://localhost/MQC");
-}
-else if(file_exists("C:/Users/quentin")){
+} else if(file_exists("C:/Users/quentin")){
     define("__LOCAL_PATH__", "http://localhost:8001/ProjectOne");
-<<<<<<< HEAD
 } elseif( file_exists("C:/Users/AifeDesPaix") ) {
     define("__LOCAL_PATH__", "http://localhost/connardedeconfluence/MQC");
-=======
-}
-elseif( file_exists("C:/Users/AifeDesPaix") ) {
-    define("__LOCAL_PATH__", "http://localhost/ProjectOne");
->>>>>>> 7168a71c92d979b313cf7952ecdfbbb42d1b5d29
-}
-else {
+} else {
     define("__LOCAL_PATH__", "http://monquartierconfluence.labo-g4.fr/MonQuartierConfluence");
 }
 define("__INCLUDE_PATH__", __LOCAL_PATH__."/php/include/");
