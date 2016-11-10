5e9dcb927f653043941f6f2f2b5c3d0582aeda7e
Shoormax <valentin.derudet2@gmail.com>
2016-11-10 10:19:39 +0100

    Petit changement dans le design et ajout d'une description pour le centre co

--- php/commerces.php
+++ php/commerces.php
@@ -9,21 +9,18 @@
 <div class="article" id="articleCommerci">
     <div class="wrap-visuel">
         <div class="wrap-titre-sous-container tcenter">
-            <span class="titre-sous-container">Le musée</span>
+            <span class="titre-sous-container">Les commerces</span>
         </div>
         <img class="imgArticle" src="img/min/musee.jpg">
     </div>
     <div class="wrap-textuel">
         <div class="sous-wrap-text tcenter">
-            <p>Le musée</p>
+            <p>Le centre commercial</p>
             <span>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis orci eget semper ornare. Etiam sed porttitor nisl. Nunc pretium semper dolor, vitae dapibus diam condimentum sed. Suspendisse tincidunt aliquet mauris, at auctor ante sagittis vel. Pellentesque vitae nisl nulla. Donec vitae accumsan orci. Duis quis enim sit amet eros consequat posuere. Quisque aliquet tincidunt sem, eu laoreet metus tristique tristique. Nunc enim leo, rhoncus vel vulputate sed, pellentesque at mi. Aliquam eget urna pulvinar, mollis ex ac, fermentum orci.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis orci eget semper ornare. Etiam sed porttitor nisl. Nunc pretium semper dolor, vitae dapibus diam condimentum sed. Suspendisse tincidunt aliquet mauris, at auctor ante sagittis vel. Pellentesque vitae nisl nulla. Donec vitae accumsan orci. Duis quis enim sit amet eros consequat posuere. Quisque aliquet tincidunt sem, eu laoreet metus tristique tristique. Nunc enim leo, rhoncus vel vulputate sed, pellentesque at mi. Aliquam eget urna pulvinar, mollis ex ac, fermentum orci
-                </span>
+                Le centre commercial de Confluence, ouvert depuis le 4 avril 2012, est situé au 112 Cours Charlemagne à Lyon 69002. <br>
+                Il comprend aujourd’hui 106 commerces de toute sorte, 12 restaurants, un restaurant, un cinéma ainsi qu’un parking de 1500 places et un hôtel <a href="http://www.novotel.com/fr/hotel-7325-novotel-lyon-confluence/index.shtml">Novotel</a> de 150 chambres.
+            </span>
         </div>
     </div>
     <div class="clearfix"></div>
-
-
-
 </div>
