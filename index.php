09bbd889da25aaa1b8298f155620a734372b1159
Shoormax <valentin.derudet2@gmail.com>
2016-11-10 11:53:03 +0100

    Légère modif

--- index.php
+++ index.php
@@ -20,32 +20,32 @@ $aPages = [
     'Contact'       => 'contact.php'
 ];
 ?>
-  <link rel="icon" type="image/png" href="img/min/Musee.png" />
-  <body>
-  <div id="menu_haut">
-    <?php
-    foreach ($aPages as $titre => $page) {
-      $sSsAccent = ucfirst(str_replace('.php', '', $page));
-      echo '<div id="link_'.$sSsAccent.'" class="nav_item">
-      <img src="img/svg/Logo'.$sSsAccent.'.svg">
-      <span class="nav_txt">'.$titre.'</span>
-      </div>';
-    }
-    ?>
-  </div>
+    <link rel="icon" type="image/png" href="img/min/Musee.png" />
+    <body>
+    <div id="menu_haut">
+        <?php
+        foreach ($aPages as $titre => $page) {
+            $sSsAccent = ucfirst(str_replace('.php', '', $page));
+            echo '<div id="link_'.$sSsAccent.'" class="nav_item">
+                  <img src="img/svg/Logo'.$sSsAccent.'.svg">
+                  <span class="nav_txt">'.$titre.'</span>
+                  </div>';
+        }
+        ?>
+    </div>
 
-  <div id="container">
-    <?php
-    foreach ($aPages as $titre => $page) {
-      $sSsAccent = ucfirst(str_replace('.php', '', $page));
-      echo $page == "accueil.php" ? '<div id="'.$sSsAccent.'" class="">' : '<div id="'.$sSsAccent.'" class="sous-container">';
-      include 'php/'.$page;
-      echo '</div>';
-    }
-    ?>
-    <a id="mentions_legales" href="php/mentions_legales.php">Mentions légales</a>
-  </div>
-  </body>
+    <div id="container">
+        <?php
+        foreach ($aPages as $titre => $page) {
+            $sSsAccent = ucfirst(str_replace('.php', '', $page));
+            echo $page == "accueil.php" ? '<div id="'.$sSsAccent.'" class="">' : '<div id="'.$sSsAccent.'" class="sous-container">';
+            include 'php/'.$page;
+            echo '</div>';
+        }
+        ?>
+        <a id="mentions_legales" href="php/mentions_legales.php">Mentions légales</a>
+    </div>
+    </body>
 
 <?php
 include_once(__INCLUDE_PATH__.'footer.php');
