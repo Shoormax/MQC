2c84b8f6f151e9d4d365183e391d98b78913f532
Quentin Benyahia <quentinbenyahia@gmail.com>
2016-12-06 10:02:08 +0100

    Boutique intégration

--- php/traitement/panier.php
+++ php/traitement/panier.php
@@ -26,25 +26,22 @@ if($user instanceof Utilisateur) {
     $panier = $user->getPanierNonValide();
     $tabRetour['html'] = '<div class="tableauPanier" id="tablePanier">';
     if($panier instanceof Panier && !empty($panier->getProduits())) {
-        $tabRetour['html'] .= 'Créé le : '.$panier->getDateAdd(true);
-        $tabRetour['html'] .= '<tr><td>Produit</td><td>Quantite</td><td>Prix unitaire</td><td>Prix total</td><td style="border: none"></td><td style="border: none"><i class="fa fa-times-circle" aria-hidden="true" onclick="affichagePanier()"></i></td></tr>';
         foreach ($panier->getProduits() as $p) {
             $produit = Produit::rechercheParId($p['id_produit']);
-            $tabRetour['html'] .= '<tr><td>'.ucfirst($produit->getLibelle()).'</td><td id="quantiteProduitPanier'.$p['id_produit'].'">'.$panier->getQuantiteProduit($p['id_produit']).'</td>
-                                    <td>'.$produit->getPrix().' €</td>
-                                    <td>'.$produit->getPrix()*$panier->getQuantiteProduit($p['id_produit']).' €</td>
-                                    <td><i class="fa fa-minus-circle" aria-hidden="true" onclick="modificationPanier('.$p['id_produit'].','.$panier->getId().', this)"></i></td>
-                                    <td><i class="fa fa-plus-circle" aria-hidden="true" onclick="modificationPanier('.$p['id_produit'].','.$panier->getId().', this)"></i></td></tr>';
+            $tabRetour['html'] .= '<div class="blocPanier">
+                                        <img class="imagePanier" src="'.$produit->getImage().'"><div>'.ucfirst($produit->getLibelle()).'</div>'.$panier->getQuantiteProduit($p['id_produit']).'
+                                        <div>'.$produit->getPrix()*$panier->getQuantiteProduit($p['id_produit']).' €</div>
+                                        <i class="fa fa-minus-circle" aria-hidden="true" onclick="modificationPanier('.$p['id_produit'].','.$panier->getId().', this)"></i>
+                                        <i class="fa fa-plus-circle" aria-hidden="true" onclick="modificationPanier('.$p['id_produit'].','.$panier->getId().', this)"></i>
+                                    </div>';
         }
-        $tabRetour['html'] .= '<td style="border: none"></td><td style="border: none"></td><td>Total</td><td>'.$panier->getTotal().' €</td>';
-        $tabRetour['html'] .= '</table>';
-        $tabRetour['html'] .= '<input class="button" type="button" id="validationPanier'.$panier->getId().'" onclick="validationPanier('.$panier->getId().', '.$_POST['id_utilisateur'].')" value="Valider">';
-        $tabRetour['html'] .= '<i class="fa fa-spinner hide" aria-hidden="true" id="validationPanierLoader'.$panier->getId().'"></i>';
+        $tabRetour['html'] .= '<div class="total"><span>Total : '.$panier->getTotal().' €</span></div>';
+        $tabRetour['html'] .= '<div><input class="button" type="button" id="validationPanier'.$panier->getId().'" onclick="validationPanier('.$panier->getId().', '.$_POST['id_utilisateur'].')" value="Valider">';
+        $tabRetour['html'] .= '<i class="fa fa-spinner hide" aria-hidden="true" id="validationPanierLoader'.$panier->getId().'"></i></div>';
 
     }
     else{
         $tabRetour['html'] = '<a onclick="affichagePanier()">Ce panier est vide.</a>';
-        $tabRetour['html'] .= '</table>';
     }
 
 }
