3040ea45654ca9810331703cb047de550356f902
Quentin Benyahia <quentinbenyahia@gmail.com>
2016-12-07 18:01:42 +0100

    Design modification produit

--- php/views/main_page/article.php
+++ php/views/main_page/article.php
@@ -13,9 +13,9 @@ $template = "<div id='".$a->getTitreNavbar()."' class='sous-container'>
                     <span class='titre-sous-container'>".$a->getTitreArticle()."</span>
                 </div>";
  $template .= $a->getTitreArticle() == 'Accessibilité' ?
-            "<button type='button' id='btn_famille'>Famille</button>
-            <button type='button' id='btn_sportif'>Sportif</button>
-            <button type='button' id='btn_culturel'>Culturel</button>
+            "<button class=\"buttonModif\" type='button' id='btn_famille'>Famille</button>
+            <button class=\"buttonModif\" type='button' id='btn_sportif'>Sportif</button>
+            <button class=\"buttonModif\" type='button' id='btn_culturel'>Culturel</button>
             <div id='carte'></div>" :
             "<img class='imgArticle' src='".$a->getImage()."'>";
             $template .= "</div>
