637f81f3837ecea0937ece67d434d664bea86b1e
clapette <clapette01@gmail.com>
2016-12-07 17:36:13 +0100

    Merge branch 'master' of https://github.com/Shoormax/MQC into JoanMerging
    # Conflicts:
    #	gestion/administration.php

--- indexSite.php
+++ indexSite.php
@@ -55,7 +55,7 @@ $articles = Article::rechercherParParam($param, 10);
             include 'php/views/main_page/article.php';
         }
         include 'php/views/main_page/contact.php';
-        $ml = $id_langue == 2 ? 'Terms and conditions' : 'Mentions légales';
+        $ml = $_COOKIE['langue'] == 2 ? 'Terms and conditions' : 'Mentions légales';
         echo'<a id="mentions_legales" href="php/views/main_page/mentions_legales.php">'.$ml.'</a>';
         ?>
     </div>
